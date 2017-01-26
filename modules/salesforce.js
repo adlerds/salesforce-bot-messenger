9d92c269d624486aad9dfdbf76d208bfeb8e17e4
adlerds <david@dadigital.com>
2017-01-26 10:33:39 -0600

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -11,7 +11,7 @@ let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
     clientSecret: SF_CLIENT_SECRET,
     redirectUri: 'http://localhost:3000/oauth/_callback',
-    environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default *Added to test if this will resolve 'instance_url' error
+    environment: 'sandbox',  // optional, salesforce 'sandbox' or 'production', production default *Added to test if this will resolve 'instance_url' error
     mode: 'single',
     autoRefresh: true
 });
