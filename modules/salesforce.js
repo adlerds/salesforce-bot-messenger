bb7de2742104527393901651245596453f6a7ff7
adlerds <david@dadigital.com>
2017-02-09 15:12:38 -0600

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -30,7 +30,7 @@ let login = () => {
 let findAccount = name => {
     return new Promise((resolve, reject) => {
         //let q = "SELECT Id, Name, BillingStreet, BillingCity, BillingState, Picture_URL__c, Phone FROM Account WHERE Name LIKE '%" + name + "%' LIMIT 5";
-          let q = "SELECT Id, Name, PersonTitle, PersonEmail FROM Account WHERE '%" + name + "%' LIMIT 5";
+          let q = "SELECT Id, Name, PersonTitle, PersonEmail FROM Account WHERE Name LIKE '%" + name + "%' LIMIT 5";
         org.query({query: q}, (err, resp) => {
             if (err) {
                 reject("An error as occurred");
